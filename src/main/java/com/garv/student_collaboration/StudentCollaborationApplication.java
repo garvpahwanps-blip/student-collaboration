e2f1a92717296231209c41d1b5e71dd161b34043
Garv Pahwa <garvpahwa.nps@gmail.com>
2026-08-22 17:29:31 +0530

    Initial student-collaboration backend

---
+++ src/main/java/com/garv/student_collaboration/StudentCollaborationApplication.java
@@ -0,0 +1,13 @@
+package com.garv.student_collaboration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCollaborationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCollaborationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garv/student_collaboration/StudentCollaborationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garv.student_collaboration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCollaborationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
