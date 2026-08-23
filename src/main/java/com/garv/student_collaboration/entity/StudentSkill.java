ef2d28d501f5f1abc029004003875c4a67683ddb
Garv Pahwa <garvpahwa.nps@gmail.com>
2026-08-23 10:59:37 +0530

    StudentSkill entity having many to one relationship with both Student and Skill

---
+++ src/main/java/com/garv/student_collaboration/entity/StudentSkill.java
@@ -0,0 +1,36 @@
+package com.garv.student_collaboration.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"student_id","skill_id"})}
+)
+@Entity
+public class StudentSkill {
+    public enum Level{
+        BEGINNER,
+        INTERMEDIATE,
+        ADVANCED,
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id", nullable = false)
+    private Skill skill;
+    @NotNull(message = "Level is required")
+    @Enumerated(EnumType.STRING)
+    private Level level;
+    private boolean learningStatus;
+
+}
